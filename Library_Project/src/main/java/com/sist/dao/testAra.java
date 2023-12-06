fa5a05cf9edcc37a3cfd77bd06eca3e6c43f4ad2
SIST <SIST@DESKTOP-H16B7VH>
2023-12-07 08:45:55 +0900

    신아라-vo제작

--- Library_Project/src/main/java/com/sist/dao/testAra.java
+++
@@ -1,7 +0,0 @@
-package com.sist.dao;
-
-public class testAra {
-	public static void main(String[] args) {
-		System.out.println("saldsadasdsadd");
-	}
-}
--- Library_Project/src/main/java/com/sist/vo/testAra.java
+++
@@ -1,7 +0,0 @@
-package com.sist.vo;
-
-public class testAra {
-	public static void main(String[] args) {
-		System.out.println("saldsadasdsadd");
-	}
-}
